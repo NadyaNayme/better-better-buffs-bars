0c95fdadd2ac1523c8c4457f0eb01242d9f12596
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2025-07-08 14:59:26 -0700

    work towards more audio

--- src/lib/alerts.ts
+++ src/lib/alerts.ts
@@ -1,7 +1,35 @@
-import OverloadAlert from "../assets/audio/Callum_Overload.mp3";
+import AnimateDead from "../assets/audio/Callum/Callum_Overload.mp3";
+import Antifire from "../assets/audio/Callum/Callum_Overload.mp3";
+import Antipoison from "../assets/audio/Callum/Callum_Overload.mp3";
+import Aura from "../assets/audio/Callum/Callum_Overload.mp3";
+import CannonDecay from "../assets/audio/Callum/Callum_Overload.mp3";
+import CantDrink from "../assets/audio/Callum/Callum_Overload.mp3";
+import Darkness from "../assets/audio/Callum/Callum_Overload.mp3";
+import DivineCharge from "../assets/audio/Callum/Callum_Overload.mp3";
+import Ectoplasm from "../assets/audio/Callum/Callum_Overload.mp3";
+import Familiar from "../assets/audio/Callum/Callum_Overload.mp3";
+import GodBook from "../assets/audio/Callum/Callum_Overload.mp3";
+import HealthLow from "../assets/audio/Callum/Callum_Overload.mp3";
+import Relentless from "../assets/audio/Callum/Callum_Overload.mp3";
+import IncenseSticks from "../assets/audio/Callum/Callum_Overload.mp3";
+import NoRunes from "../assets/audio/Callum/Callum_Overload.mp3";
+import Overload from "../assets/audio/Callum/Callum_Overload.mp3";
+import Payment from "../assets/audio/Callum/Callum_Overload.mp3";
+import PrayerEmpty from "../assets/audio/Callum/Callum_Overload.mp3";
+import PrayerLow from "../assets/audio/Callum/Callum_Overload.mp3";
+import PrayerRenewal from "../assets/audio/Callum/Callum_Overload.mp3";
+import Runes from "../assets/audio/Callum/Callum_Overload.mp3";
+import Scream from "../assets/audio/Callum/Callum_Overload.mp3";
+import SipPrayer from "../assets/audio/Callum/Callum_Overload.mp3";
+import StrikeAgain from "../assets/audio/Callum/Callum_Overload.mp3";
+import SummoningPoints from "../assets/audio/Callum/Callum_Overload.mp3";
+import WeaponPoison from "../assets/audio/Callum/Callum_Overload.mp3";
+import YouHaveDied from "../assets/audio/Callum/Callum_Overload.mp3";
 
 export const alertsMap: Record<string,string> = {
-    'Overload': OverloadAlert,
-    'Supreme Overload': OverloadAlert,
-    'Elder Overload': OverloadAlert,
+    'Overload': Overload,
+    'Supreme Overload': Overload,
+    'Elder Overload': Overload,
+    'Aura': Aura,
+    'Darkness': Darkness,
 }
