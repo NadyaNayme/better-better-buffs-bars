2ea1f36f9d451e5e7ae99a9aefefe157dc1fa9b9
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2025-07-08 15:17:31 -0700

    Fix audio sources

--- src/lib/alerts.ts
+++ src/lib/alerts.ts
@@ -1,30 +1,29 @@
-import AnimateDead from "../assets/audio/Callum/Callum_Overload.mp3";
-import Antifire from "../assets/audio/Callum/Callum_Overload.mp3";
-import Antipoison from "../assets/audio/Callum/Callum_Overload.mp3";
-import Aura from "../assets/audio/Callum/Callum_Overload.mp3";
-import CannonDecay from "../assets/audio/Callum/Callum_Overload.mp3";
-import CantDrink from "../assets/audio/Callum/Callum_Overload.mp3";
-import Darkness from "../assets/audio/Callum/Callum_Overload.mp3";
-import DivineCharge from "../assets/audio/Callum/Callum_Overload.mp3";
-import Ectoplasm from "../assets/audio/Callum/Callum_Overload.mp3";
-import Familiar from "../assets/audio/Callum/Callum_Overload.mp3";
-import GodBook from "../assets/audio/Callum/Callum_Overload.mp3";
-import HealthLow from "../assets/audio/Callum/Callum_Overload.mp3";
-import Relentless from "../assets/audio/Callum/Callum_Overload.mp3";
-import IncenseSticks from "../assets/audio/Callum/Callum_Overload.mp3";
-import NoRunes from "../assets/audio/Callum/Callum_Overload.mp3";
+import AnimateDead from "../assets/audio/Callum/Callum_Animate Dead.mp3";
+import Antifire from "../assets/audio/Callum/Callum_Antifire.mp3";
+import Antipoison from "../assets/audio/Callum/Callum_AntiPoison.mp3";
+import Aura from "../assets/audio/Callum/Callum_Aura.mp3";
+import CannonDecay from "../assets/audio/Callum/Callum_Cannon Decay.mp3";
+import CantDrink from "../assets/audio/Callum/Callum_Cant Drink.mp3";
+import Darkness from "../assets/audio/Callum/Callum_Darkness.mp3";
+import DivineCharge from "../assets/audio/Callum/Callum_Divine Charge.mp3";
+import Ectoplasm from "../assets/audio/Callum/Callum_Ectoplasm.mp3";
+import Familiar from "../assets/audio/Callum/Callum_Familiar.mp3";
+import GodBook from "../assets/audio/Callum/Callum_God Book.mp3";
+import HealthLow from "../assets/audio/Callum/Callum_Health Low.mp3";
+import Relentless from "../assets/audio/Callum/Callum_ImRelentless.mp3";
+import IncenseSticks from "../assets/audio/Callum/Callum_Incense Sticks.mp3";
+import NoRunes from "../assets/audio/Callum/Callum_NoRunes.mp3";
 import Overload from "../assets/audio/Callum/Callum_Overload.mp3";
-import Payment from "../assets/audio/Callum/Callum_Overload.mp3";
-import PrayerEmpty from "../assets/audio/Callum/Callum_Overload.mp3";
-import PrayerLow from "../assets/audio/Callum/Callum_Overload.mp3";
-import PrayerRenewal from "../assets/audio/Callum/Callum_Overload.mp3";
-import Runes from "../assets/audio/Callum/Callum_Overload.mp3";
-import Scream from "../assets/audio/Callum/Callum_Overload.mp3";
-import SipPrayer from "../assets/audio/Callum/Callum_Overload.mp3";
-import StrikeAgain from "../assets/audio/Callum/Callum_Overload.mp3";
-import SummoningPoints from "../assets/audio/Callum/Callum_Overload.mp3";
-import WeaponPoison from "../assets/audio/Callum/Callum_Overload.mp3";
-import YouHaveDied from "../assets/audio/Callum/Callum_Overload.mp3";
+import Payment from "../assets/audio/Callum/Callum_Payment.mp3";
+import PrayerEmpty from "../assets/audio/Callum/Callum_Prayer Empty.mp3";
+import PrayerLow from "../assets/audio/Callum/Callum_Prayer Low.mp3";
+import PrayerRenewal from "../assets/audio/Callum/Callum_Prayer Renewal.mp3";
+import Runes from "../assets/audio/Callum/Callum_Runes.mp3";
+import Scream from "../assets/audio/Callum/Callum_Scream.mp3";
+import SipPrayer from "../assets/audio/Callum/Callum_Sip Prayer.mp3";
+import StrikeAgain from "../assets/audio/Callum/Callum_Strike Again.mp3";
+import SummoningPoints from "../assets/audio/Callum/Callum_Summoning Points.mp3";
+import WeaponPoison from "../assets/audio/Callum/Callum_Weapon Poison.mp3";
 
 export const alertsMap: Record<string,string> = {
     'Overload': Overload,
@@ -32,4 +31,5 @@ export const alertsMap: Record<string,string> = {
     'Elder Overload': Overload,
     'Aura': Aura,
     'Darkness': Darkness,
+    'Weapon Poison': WeaponPoison,
 }
