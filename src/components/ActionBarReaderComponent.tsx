1792922ab49daba1b55c9dd1f96c91d033fb3f8e
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2025-07-08 12:14:20 -0700

    Add debug log again

--- src/components/ActionBarReaderComponent.tsx
+++ src/components/ActionBarReaderComponent.tsx
@@ -26,6 +26,7 @@ export function ActionBarReaderComponent({
   const readAbilities = useCallback(() => {
     if (readerRef.current) {
       const data = readerRef.current.readLife();
+      console.log(data);
       if (data) {
         checkCombat(data);
         setLifeData({
