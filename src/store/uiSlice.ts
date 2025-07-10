6187d960310d43c71105bfa7bd47eb1d47b25d1b
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2025-07-09 19:28:58 -0700

    fix: add back my debug strings

--- src/store/uiSlice.ts
+++ src/store/uiSlice.ts
@@ -19,12 +19,19 @@ export interface UISlice {
 }
 
 const debugStrings = [
-  "You have entered the matrix.",
-  "You are now behind the scenes.",
-  "Be careful poking around here...",
-  "4|23 `/0(_) 4 1337 |-|4><0|2?",
-  "Welcome to Debug Mode: where the bugs are the features.",
-];
+    "You have entered the matrix.",
+    "You are now behind the scenes.",
+    "Be careful poking around here...",
+    "4|23 `/0(_) 4 1337 |-|4><0|2?",
+    "01000100 01100101 01100010 01110101 01100111 00100000 01001101 01101111 01100100 01100101 00100000 01000101 01101110 01100001 01100010 01101100 01100101 01100100",
+    "Welcome to Debug Mode: where the bugs are the features.",
+    "Ah, Debug Mode. Also known as \"guess and check\" mode.",
+    "Debug Mode activated. Please refrain from screaming at the monitor.",
+    "Enabling Debug Mode... okay, now which of these console logs is lying to me?",
+    "Debug Mode: because \"it works on my machine\" wasn't nihilistic enough.",
+    "Congratulations! Your Debugging level is now 79! Or was it 97?",
+    "Welcome to Debug Mode. Blame Nyu that you're here.",
+  ]
 
 export const createUISlice: StateCreator<Store, [], [], UISlice> = (set, get) => ({
   debugMode: false,
