ecc3fa8decd94a5187f46780d365e538334a217b
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2025-07-10 20:07:38 -0700

    reduce margin by a lot

--- src/components/AddBuffModal.tsx
+++ src/components/AddBuffModal.tsx
@@ -142,7 +142,7 @@ const AddBuffModal: React.FC<AddBuffModalComponentProps> = ({ groupId, onClose }
 
         <div className="flex flex-col justify-start mt-6">
           <p>Some buffs have been temporarily disabled from being added to new groups while I attempt to fix a flickering issue they cause. If you have these buffs tracking in a group and are experiencing issues it is recommended that you remove them.</p>
-          <ul className="list-disc ml-30">
+          <ul className="list-disc ml-10">
             <li>Conjure Undead Army</li>
             <li>Death Spark</li>
             <li>DPS Prayers</li>
