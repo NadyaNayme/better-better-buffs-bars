04fceec78b4383da450cfa3e512eb36520a0d777
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2025-07-19 10:23:31 -0700

    no idea how this works tbh, it doesn't for me.

--- src/lib/storeMonitor.ts
+++ src/lib/storeMonitor.ts
@@ -2,6 +2,8 @@ import { create } from 'zustand'
 import { subscribeWithSelector } from 'zustand/middleware'
 import useStore from '../store/store'
 
+// https://github.com/pmndrs/zustand?tab=readme-ov-file#using-subscribe-with-selector
+
 const monitorStore = create(
   subscribeWithSelector(() => (useStore.getState().group.enabled))
 )
