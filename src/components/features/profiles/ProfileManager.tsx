e676f7b2b902cc0b9d950fe16be3c7983400a46b
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2025-07-18 23:23:39 -0700

    .

--- src/components/features/profiles/ProfileManager.tsx
+++ src/components/features/profiles/ProfileManager.tsx
@@ -79,7 +79,7 @@ const ProfileManager: React.FC<ProfileManagerProps> = ({ openModalForProfile }) 
                     <button onClick={handleSaveEdit} className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded">
                       Save
                     </button>
-                    <button onClick={() => setEditingProfile('')} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded">
+                    <button onClick={() => setEditingProfile('')} className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
                       Cancel
                     </button>
                   </>
